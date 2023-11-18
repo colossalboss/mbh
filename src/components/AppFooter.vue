3b505376d44281bcdd5910bdfe6c37f70479b106
Godstar <sengodstar@gmail.com>
2023-11-18 01:39:12 +0100

    Add about us content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,6 +3,7 @@
       <div class="w-full text-center">
         <p class="text-white text-sm">18+ Adults only. Don't let gambling become a problem in your life. Check our responsible gambling page for more info.</p>
         <a href="#" class="p-4 text-smlg text-gray-500">&copy; Copyright. MyBetHaven 2023, All rights reserved</a><br>
+        <router-link to="/about" class="p-4 text-smlg text-white">About us</router-link><br>
         <router-link to="/privacy" class="p-4 text-smlg text-gray-500">Privacy Policy</router-link>
       </div>
     </div>
