3a667b0d7df60ee4757e07bbf2f92ece1d2c0c34
Godstar <sengodstar@gmail.com>
2023-11-24 02:47:10 +0100

    Fix table row display in small screens

--- src/components/PredictionRow.vue
+++ src/components/PredictionRow.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="md:flex p-0 m-0">
-        <div class="md:w-2/12 sm:flex justify-between p-4 md:bg-regalblue">
+        <div class="md:w-2/12 flex justify-between p-4 md:bg-regalblue">
             <span class="font-bold md:hidden">Time: &nbsp;</span>
             <a href="#" class="text-sm md:text-white">{{new Date( prediction.startAt).toLocaleDateString() }} {{ new Date(prediction.startAt).toLocaleTimeString()}}</a>
         </div>
-        <div class="md:w-5/12  sm:flex justify-between border p-4">
+        <div class="md:w-5/12  flex justify-between border p-4">
             <span class="font-bold md:hidden">Fixture: &nbsp;</span>
             <a href="#" class="text-sm">{{ prediction.homeTeamName }} vs {{ prediction.awayTeamName }}</a>
         </div>
-        <div class="md:w-2/12  sm:flex justify-between border p-4 md:block text-center">
+        <div class="md:w-2/12 flex justify-between border p-4 md:block text-center">
             <span class="font-bold md:hidden">Bet: &nbsp;</span>
             <a href="#" class="text-sm flex items-center justify-center text-center"><span>{{ prediction.betCategory }}</span><br><span>{{ prediction.bet }}</span></a>
         </div>
-        <div class="md:w-1/12  sm:flex justify-between border p-4 md:block text-center ">
+        <div class="md:w-1/12 flex justify-between border p-4 md:block text-center ">
             <span class="font-bold md:hidden">Odd: &nbsp;</span>
             <a href="#" class="text-sm text-center">{{ prediction.odd }}</a>
         </div>
-        <div class="md:w-1/12  sm:flex justify-between border p-4 md:block text-center ">
+        <div class="md:w-1/12  flex justify-between border p-4 md:block text-center ">
             <span class="font-bold md:hidden">Score: &nbsp;</span>
             <a href="#" class="text-sm text-center">{{ prediction.score }}</a>
         </div>
-        <div class="md:w-1/12  sm:flex justify-between p-4 md:block text-center  md:bg-gray-300">
+        <div class="md:w-1/12 flex justify-between p-4 md:block text-center  md:bg-gray-300">
             <span class="font-bold md:hidden">Outcome: &nbsp;</span>
             <a href="#" class="text-sm text-center capitalize font-semibold" :class="[outcomeColor(prediction.outcome.toLowerCase()) ]">{{ capitalize(prediction.outcome) }}</a>
         </div>
